aab8ca4559f817bb65a2c3320d41531abff6b7e4
Thunder Bolt <bolt.thunderbolt168@gmail.com>
2024-07-03 21:15:28 +0700

    Adding some interactive word to user with random words

--- Main.py
+++ Main.py
@@ -3,14 +3,23 @@ import random;
 ## Guess the number
 
 myNumber = random.randint(0, 9); #using randint (random integer) and fill in the min and max number
-print("I have guess the number from 0 - 9. Can you guess it? 😶");
+print(myNumber);
+askList = ["Can you guess it? 😁", "Do you wanna play with me? 🙂", "Lets play and beat me! 😎", "Can you beat me? 😏"];
+wrongAnswerRespondList = ["No Problem. Just try again😉", "Oh... Come On 😔", "Its ok, choose the number again", "Try again until you get it!"];
+correctAnswerRespondList = ["Yes... you get it 🥳", "Not bad 😉", "Nice 😁", "Congrats... 🎉💯"];
+
+randomAsk = random.choice(askList);
+print(f"Hi, what's up? i have a number in my Mind, just guess from 0 - 9. {randomAsk}");
 
 while True:
+  # Ditaruh disini supaya dapat kalimat jawaban benar dan salah secara random
+  randomWrongAnswer = random.choice(wrongAnswerRespondList);
+  randomCorrectAnswer = random.choice(correctAnswerRespondList);
   userInput = int(input("\n Enter here\n-> ")); # user will put the number
   if userInput == myNumber:
-    print("Congrats, you made it!");
+    print(f"{randomCorrectAnswer}");
     break;
   elif myNumber > userInput:
-    print("My number is greater than the number you entered. Try Again! 😏", end="\n\n");
+    print(f"My number is greater than the number you entered. {randomWrongAnswer}", end="\n\n");
   else:
-    print("My number is smaller than the number you entered. Try Again! 😏", end="\n\n");
+    print(f"My number is smaller than the number you entered. {randomWrongAnswer}", end="\n\n");
