b2c71703f9855c3e33fc7a484386355a830d9d8d
Thunder Bolt <bolt.thunderbolt168@gmail.com>
2024-07-04 12:09:57 +0700

    Adding time and score after getting the correct answer

--- Main.py
+++ Main.py
@@ -1,11 +1,14 @@
 # Library
 import random;
+import time;
 ## Guess the number
 
-myNumber = random.randint(0, 9); #using randint (random integer) and fill in the min and max number
+randomNumber = random.randint(0, 9); #using randint (random integer) and fill in the min and max number
+startTime = time.time();
+totalAnswer = 0;
 
 askList = ["Can you guess it? 😁", "Do you wanna play with me? 🙂", "Lets play and beat me! 😎", "Can you beat me? 😏"];
-wrongAnswerRespondList = ["No Problem. Just try again😉", "Oh... Come On 😔", "Its ok, choose the number again", "Try again until you get it!"];
+wrongAnswerRespondList = ["No Problem. Just try again😉", "Oh... Come On 😔", "Its ok, choose the number again", "Try again until you get it!", "GG 😎"];
 correctAnswerRespondList = ["Yes... you get it 🥳", "Not bad 😉", "Nice 😁", "Congrats... 🎉💯"];
 
 randomAsk = random.choice(askList);
@@ -18,14 +21,21 @@ while True:
 
   try:
     print("\n- Press ctrl + C for Quit");
-    userInput = int(input("\nEnter here  -> ")); # user will put the number
+    userInput = int(input("Enter here  -> ")); # user will put the number
+    totalAnswer += 1;
     if userInput < 0 or userInput > 9:
       print("Please enter between 0 and 9");
     else:
-      if userInput == myNumber:
+      if userInput == randomNumber:
         print(f"{randomCorrectAnswer}");
+        if userInput == randomNumber:
+          timeOver = time.time(); # catat waktu selesai menebak
+          totalTime = timeOver - startTime;
+          score = 10 - totalAnswer;
+          print(f"Your time : {totalTime: .2f}");
+          print(f"Your score : {score: .2f}");
         break;
-      elif myNumber > userInput:
+      elif randomNumber > userInput:
         print(f"My number is greater than the number you entered. {randomWrongAnswer}", end="\n\n");
       else:
         print(f"My number is smaller than the number you entered. {randomWrongAnswer}", end="\n\n");
