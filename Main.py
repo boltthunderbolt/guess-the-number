7f9f9a83da61e3c868108b188a86f48a61488856
Thunder Bolt <bolt.thunderbolt168@gmail.com>
2024-07-04 10:05:31 +0700

    Added user input validation if entering numbers outside the minimum and maximum limits for numbers played

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import random;
 ## Guess the number
 
 myNumber = random.randint(0, 9); #using randint (random integer) and fill in the min and max number
-print(myNumber);
+
 askList = ["Can you guess it? 😁", "Do you wanna play with me? 🙂", "Lets play and beat me! 😎", "Can you beat me? 😏"];
 wrongAnswerRespondList = ["No Problem. Just try again😉", "Oh... Come On 😔", "Its ok, choose the number again", "Try again until you get it!"];
 correctAnswerRespondList = ["Yes... you get it 🥳", "Not bad 😉", "Nice 😁", "Congrats... 🎉💯"];
@@ -15,11 +15,19 @@ while True:
   # Ditaruh disini supaya dapat kalimat jawaban benar dan salah secara random
   randomWrongAnswer = random.choice(wrongAnswerRespondList);
   randomCorrectAnswer = random.choice(correctAnswerRespondList);
-  userInput = int(input("\n Enter here\n-> ")); # user will put the number
-  if userInput == myNumber:
-    print(f"{randomCorrectAnswer}");
-    break;
-  elif myNumber > userInput:
-    print(f"My number is greater than the number you entered. {randomWrongAnswer}", end="\n\n");
-  else:
-    print(f"My number is smaller than the number you entered. {randomWrongAnswer}", end="\n\n");
+
+  try:
+    print("\n- Press ctrl + C for Quit");
+    userInput = int(input("\nEnter here  -> ")); # user will put the number
+    if userInput < 0 or userInput > 9:
+      print("Please enter between 0 and 9");
+    else:
+      if userInput == myNumber:
+        print(f"{randomCorrectAnswer}");
+        break;
+      elif myNumber > userInput:
+        print(f"My number is greater than the number you entered. {randomWrongAnswer}", end="\n\n");
+      else:
+        print(f"My number is smaller than the number you entered. {randomWrongAnswer}", end="\n\n");
+  except ValueError:
+    print("Please enter type number!");
